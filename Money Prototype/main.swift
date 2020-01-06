57e95271a21d2d296ff290f5da36a1fae5926d72
Gamer4life Puvilland <uyenwontanswer@gmail.com>
2020-01-06 11:44:08 -0800

    Test

--- Money Prototype/main.swift
+++ Money Prototype/main.swift
@@ -2,11 +2,43 @@
 //  main.swift
 //  Money Prototype
 //
-//  Created by LeUyen Pham on 1/6/20.
+//  Created by Leo Puvilland on 1/6/20.
 //  Copyright © 2020 Leo Commandblocks. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+func saveV(name:String, text:String) -> String {
+    let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    let fileURL = DocumentDirURL.appendingPathComponent(name).appendingPathExtension("txt")
+    do {
+        // Write to the file
+        try text.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+    } catch let error as NSError {
+        print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+    }
+    return fileURL.absoluteString
+}
+
+func readV(fileURL: String) -> String {
+    var readString = "" // Used to store the file contents
+    do {
+        // Read the file contents
+        readString = try String(contentsOf: NSURL(fileURLWithPath: fileURL) as URL)
+    } catch let error as NSError {
+        print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+    }
+    return readString
+}
+
+
+let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+let fileURL1 = DocumentDirURL.appendingPathComponent("date").appendingPathExtension("txt")
+if FileManager.default.fileExists(atPath: fileURL1.absoluteString) {
+    let startDate = readV(fileURL: fileURL1.absoluteString)
+} else {
+    FileManager.default.createFile(atPath: fileURL1.absoluteString, contents: "".data(using: String.Encoding.ascii))
+    let startDate = Date()
+}
+
 
